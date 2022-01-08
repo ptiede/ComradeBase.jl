0a90afce9bc486318a93bfd70ad92e86f8d29316
Documenter.jl <documenter@juliadocs.github.io>
2022-01-08 04:20:33 +0000

    build based on de8b393

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ROSEBase","category":"page"},{"location":"#ROSEBase","page":"Home","title":"ROSEBase","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ROSEBase.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ROSEBase]","category":"page"}]
+}
