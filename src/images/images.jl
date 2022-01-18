b74766f3b6e1f44a52eeba54d33ec673d49657ea
Paul Tiede <ptiede91@gmail.com>
2022-01-17 23:30:50 -0500

    Cleaned up parts of the interface

--- src/images/images.jl
+++ src/images/images.jl
@@ -2,6 +2,34 @@ abstract type AbstractIntensityMap{T,S} <: AbstractMatrix{T} end
 
 #abstract type AbstractPolarizedMap{I,Q,U,V} end
 
+@inline function intensitymap(s::M,
+                              fovx::Number, fovy::Number,
+                              nx::Int, ny::Int;
+                              pulse=ComradeBase.DeltaPulse()) where {M}
+    return intensitymap(imanalytic(M), s, fovx, fovy, nx, ny; pulse)
+end
+
+function intensitymap(::IsAnalytic, s, fovx::Number, fovy::Number, nx::Int, ny::Int; pulse=ComradeBase.DeltaPulse())
+    px = fovx/max(nx-1,1)
+    py = fovy/max(ny-1,1)
+    pimg = map(CartesianIndices((1:nx,1:nx))) do I
+        iy,ix = Tuple(I)
+        x = -fovx/2 + (ix-1)*px
+        y = -fovy/2 + (iy-1)*py
+        f = intensity_point(s, x, y)
+        return f
+    end
+    return IntensityMap(pimg, fovx, fovy, pulse)
+end
+
+function intensitymap!(::IsAnalytic, im::AbstractIntensityMap, m)
+    xitr, yitr = imagepixels(im)
+    @inbounds for (i,x) in pairs(xitr), (j,y) in pairs(yitr)
+        im[j, i] = intensity_point(m, x, y)
+    end
+    return im
+end
+
 
 """
 Pulse
