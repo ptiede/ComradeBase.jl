ccdbf9040d0ce53c3a80e8b2f033cc94dec20e21
Paul Tiede <ptiede91@gmail.com>
2022-02-26 09:37:46 -0500

    changed location of pulse

--- src/images/pulse.jl
+++ src/images/pulse.jl
@@ -1,5 +1,24 @@
 export DeltaPulse, SqExpPulse, BSplinePulse
 
+"""
+Pulse
+Pixel response function for a radio image model. This makes
+a discrete sampling continuous by picking a certain *smoothing*
+kernel for the image.
+
+# Notes
+To see the implemented Pulses please use the subtypes function i.e.
+`subtypes(Pulse)`
+"""
+abstract type Pulse <: AbstractModel end
+
+visanalytic(::Type{<:Pulse}) = IsAnalytic()
+imanalytic(::Type{<:Pulse}) = IsAnalytic()
+isprimitive(::Type{<:Pulse}) = IsPrimitive()
+
+@inline intensity_point(p::Pulse, x,y) = κ(p::Pulse, x)*κ(p::Pulse, y)
+@inline visibility_point(p::Pulse, u,v) = ω(p::Pulse, u)*ω(p::Pulse, v)
+
 
 flux(p::Pulse) = κflux(p)^2
 
