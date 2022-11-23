c7992adf046527d41549f287fb333d0b0b6081ea
Paul Tiede <ptiede91@gmail.com>
2022-11-22 20:36:31 -0500

    fix another bug

--- src/images/polarizedtypes.jl
+++ src/images/polarizedtypes.jl
@@ -206,7 +206,7 @@ Compute the fractional linear polarization of a stokes vector
 or coherency matrix
 """
 m̆(m::StokesVector{T}) where {T} = (m.Q + 1im*m.U)/(m.I + eps(T))
-m̆(m::CoherencyMatrix{CircBasis,CircBasis,T}) = 2*m.e12/(m.e11+m.e22)
+m̆(m::CoherencyMatrix{CircBasis,CircBasis}) = 2*m.e12/(m.e11+m.e22)
 
 """
     $(SIGNATURES)
