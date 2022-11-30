f83d50c19ca2ce0e3c31918c21da17ffc8693388
Paul Tiede <ptiede91@gmail.com>
2022-11-29 22:32:49 -0500

    update to dimensional data

--- src/images/images.jl
+++ src/images/images.jl
@@ -1,13 +1,13 @@
-export IntensityMap, ImageDimensions, named_dims, dims, grid,
-       named_axiskeys, imagepixels, pixelsizes, fov
-include("keyed_image.jl")
+# export IntensityMap, ImageDimensions, named_dims, dims, grid,
+#        named_axiskeys, imagepixels, pixelsizes, fov
+# include("keyed_image.jl")
 
-export StokesIntensityMap, stokes
-include("stokes_image.jl")
+#export StokesIntensityMap, stokes
+#include("stokes_image.jl")
+
+export IntensityMap, named_dims, dims, imagepixels, pixelsizes, fieldofview, StokesIntensityMap
+include("dim_image.jl")
+
 
 export flux, centroid, second_moment
 include("moments.jl")
-
-
-export DimIntensityMap
-include("dim_image.jl")
