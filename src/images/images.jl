4be23ae6ffffa468112988915b5501cc63fa6c40
Paul Tiede <ptiede91@gmail.com>
2022-11-30 11:17:53 -0500

    update back to AxisKeys

--- src/images/images.jl
+++ src/images/images.jl
@@ -1,13 +1,9 @@
-# export IntensityMap, ImageDimensions, named_dims, dims, grid,
-#        named_axiskeys, imagepixels, pixelsizes, fov
-# include("keyed_image.jl")
+ export IntensityMap, ImageDimensions,
+        named_axiskeys, imagepixels, pixelsizes, imagegrid
+ include("keyed_image.jl")
 
-#export StokesIntensityMap, stokes
-#include("stokes_image.jl")
-
-export IntensityMap, named_dims, dims, imagepixels, pixelsizes, fieldofview, StokesIntensityMap
-include("dim_image.jl")
-
+export StokesIntensityMap, stokes
+include("stokes_image.jl")
 
 export flux, centroid, second_moment
 include("moments.jl")
