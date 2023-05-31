d293a97ccb0998dd05ab37d142b133f6f1cd1964
Paul Tiede <ptiede91@gmail.com>
2023-05-31 08:59:52 -0400

    Add io

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,9 @@
 using ComradeBase, StaticArrays, JET
+using Pyehtim
 using Test
 
 @testset "ComradeBase.jl" begin
     include(joinpath(@__DIR__, "polarizedtypes.jl"))
     include(joinpath(@__DIR__, "images.jl"))
+    include(joinpath(@__DIR__, "io.jl"))
 end
