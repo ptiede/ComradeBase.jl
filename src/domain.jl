627ded501b71f05dabfe1452098d4da1010001a5
Paul Tiede <ptiede91@gmail.com>
2024-04-25 14:39:32 -0400

    update docstring

--- src/domain.jl
+++ src/domain.jl
@@ -382,7 +382,8 @@ end
 
 Builds an unstructured grid (really a vector of points) from the dimensions `dims`.
 The `executor` is used controls how the grid is computed when calling
-`visibilitymap` or `intensitymap`.
+`visibilitymap` or `intensitymap`. The default is `Serial` which mean regular CPU computations.
+For threaded execution use [`ThreadsEx()`](@ref) or load `OhMyThreads.jl` to uses their schedulers.
 
 Note that unlike `RectiGrid` which assigns dimensions to the grid points, `UnstructuredDomain`
 does not. This is becuase the grid is unstructured the points are a cloud in a space
