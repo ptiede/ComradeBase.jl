3bf6eafef2978a3ffbe31a056a165c501b787cd2
Paul Tiede <ptiede91@gmail.com>
2024-08-16 23:04:05 -0400

    Format every file

--- ext/ComradeBaseEnzymeExt.jl
+++ ext/ComradeBaseEnzymeExt.jl
@@ -5,18 +5,24 @@ using Enzyme: @parallel
 
 const EnzymeThreads = ComradeBase.ThreadsEx{:Enzyme}
 
-function ComradeBase.intensitymap_analytic!(img::IntensityMap{T, N, D, <:ComradeBase.AbstractRectiGrid{D, <:EnzymeThreads}}, s::ComradeBase.AbstractModel) where {T, N, D}
+function ComradeBase.intensitymap_analytic!(img::IntensityMap{T,N,D,
+                                                              <:ComradeBase.AbstractRectiGrid{D,
+                                                                                              <:EnzymeThreads}},
+                                            s::ComradeBase.AbstractModel) where {T,N,D}
     dx, dy = ComradeBase.pixelsizes(img)
     g = ComradeBase.domainpoints(img)
     f = Base.Fix1(ComradeBase.intensity_point, s)
     pimg = parent(img)
     @parallel for I in CartesianIndices(pimg)
-        pimg[I] = f(g[I])*dx*dy
+        pimg[I] = f(g[I]) * dx * dy
     end
     return nothing
 end
 
-function ComradeBase.intensitymap_analytic!(img::UnstructuredMap{T, N, <:ComradeBase.UnstructuredDomain{D, <:EnzymeThreads}}, s::ComradeBase.AbstractModel) where {T, N, D}
+function ComradeBase.intensitymap_analytic!(img::UnstructuredMap{T,N,
+                                                                 <:ComradeBase.UnstructuredDomain{D,
+                                                                                                  <:EnzymeThreads}},
+                                            s::ComradeBase.AbstractModel) where {T,N,D}
     g = ComradeBase.domainpoints(img)
     f = Base.Fix1(ComradeBase.intensity_point, s)
     pimg = parent(img)
@@ -26,7 +32,10 @@ function ComradeBase.intensitymap_analytic!(img::UnstructuredMap{T, N, <:Comrade
     return nothing
 end
 
-function ComradeBase.visibilitymap_analytic!(vis::ComradeBase.FluxMap2{T,N,<:ComradeBase.AbstractSingleDomain{<:Any, <:EnzymeThreads}}, s::ComradeBase.AbstractModel) where {T,N}
+function ComradeBase.visibilitymap_analytic!(vis::ComradeBase.FluxMap2{T,N,
+                                                                       <:ComradeBase.AbstractSingleDomain{<:Any,
+                                                                                                          <:EnzymeThreads}},
+                                             s::ComradeBase.AbstractModel) where {T,N}
     dims = axisdims(vis)
     g = domainpoints(dims)
     f = Base.Fix1(ComradeBase.visibility_point, s)
--- ext/ComradeBaseOhMyThreadsExt.jl
+++ ext/ComradeBaseOhMyThreadsExt.jl
@@ -3,7 +3,10 @@ module ComradeBaseOhMyThreadsExt
 using ComradeBase
 using OhMyThreads
 
-function ComradeBase.intensitymap_analytic!(img::IntensityMap{T,N,D,<:ComradeBase.AbstractRectiGrid{D, <:OhMyThreads.Scheduler}}, s::ComradeBase.AbstractModel) where {T,N,D}
+function ComradeBase.intensitymap_analytic!(img::IntensityMap{T,N,D,
+                                                              <:ComradeBase.AbstractRectiGrid{D,
+                                                                                              <:OhMyThreads.Scheduler}},
+                                            s::ComradeBase.AbstractModel) where {T,N,D}
     dims = axisdims(img)
     dx = step(dims.X)
     dy = step(dims.Y)
@@ -11,30 +14,35 @@ function ComradeBase.intensitymap_analytic!(img::IntensityMap{T,N,D,<:ComradeBas
     pimg = parent(img)
     f = Base.Fix1(ComradeBase.intensity_point, s)
     tforeach(CartesianIndices(pimg); scheduler=executor(dims)) do I
-        pimg[I] = f(g[I])*dx*dy
+        return pimg[I] = f(g[I]) * dx * dy
     end
     return nothing
 end
 
-
-function ComradeBase.intensitymap_analytic!(img::UnstructuredMap{T,<:AbstractVector,<:UnstructuredDomain{D, <:OhMyThreads.Scheduler}}, s::ComradeBase.AbstractModel) where {T,D}
+function ComradeBase.intensitymap_analytic!(img::UnstructuredMap{T,<:AbstractVector,
+                                                                 <:UnstructuredDomain{D,
+                                                                                      <:OhMyThreads.Scheduler}},
+                                            s::ComradeBase.AbstractModel) where {T,D}
     dims = axisdims(img)
     g = domainpoints(dims)
     f = Base.Fix1(ComradeBase.intensity_point, s)
     pimg = parent(img)
     tforeach(CartesianIndices(pimg); scheduler=executor(dims)) do I
-        pimg[I] = f(g[I])
+        return pimg[I] = f(g[I])
     end
     return nothing
 end
 
-function ComradeBase.visibilitymap_analytic!(vis::ComradeBase.FluxMap2{T,N,<:ComradeBase.AbstractSingleDomain{<:Any, <: OhMyThreads.Scheduler}}, s::ComradeBase.AbstractModel) where {T,N}
+function ComradeBase.visibilitymap_analytic!(vis::ComradeBase.FluxMap2{T,N,
+                                                                       <:ComradeBase.AbstractSingleDomain{<:Any,
+                                                                                                          <:OhMyThreads.Scheduler}},
+                                             s::ComradeBase.AbstractModel) where {T,N}
     dims = axisdims(vis)
     g = domainpoints(dims)
     f = Base.Fix1(ComradeBase.visibility_point, s)
     pvis = parent(vis)
     tforeach(CartesianIndices(pvis); scheduler=executor(dims)) do I
-        pvis[I] = f(g[I])
+        return pvis[I] = f(g[I])
     end
     return nothing
 end
