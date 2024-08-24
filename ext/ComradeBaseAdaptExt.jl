087125cf5bb081e3ec0c73bae33b3bdd13deb693
Paul Tiede <ptiede91@gmail.com>
2024-08-23 20:58:19 -0400

    Add better adapt structure

--- ext/ComradeBaseAdaptExt.jl
+++ ext/ComradeBaseAdaptExt.jl
@@ -1,19 +1,29 @@
 module ComradeBaseAdaptExt
 
 using ComradeBase
+using DimensionalData
 using Adapt
+
+function Adapt.adapt_structure(to, A::IntensityMap)
+    rebuild(A, 
+        Adapt.adapt_structure(to, parent(A)),
+        Adapt.adapt_structure(to, dims(A)),
+        Adapt.adapt_structure(to, refdims(A)),
+        DimensionalData.Name(name(A)),
+        Adapt.adapt_structure(to, metadata(A))
+    )
+end
 
 function Adapt.adapt_structure(to, A::UnstructuredMap)
     return UnstructuredMap(Adapt.adapt_structure(to, parent(A)),
                            Adapt.adapt_structure(to, axisdims(A)))
 end
 
-function Adapt.adapt_structure(to, A::UnstructuredDomain)
-    return UnstructuredDomain(Adapt.adapt_structure(to, dims(A)), executor(A), header(A))
-end
-
-function Adapt.adapt_structure(to, A::RectiGrid)
-    return RectiGrid(Adapt.adapt_structure(to, dims(A)), executor(A), header(A))
+function Adapt.adapt_structure(to, A::AbstractSingleDomain)
+    return UnstructuredDomain(Adapt.adapt_structure(to, dims(A)), 
+                              Adapt.adapt_structure(executor(A)), 
+                              Adapt.adapt_structure(header(A))
+                              )
 end
 
 end
