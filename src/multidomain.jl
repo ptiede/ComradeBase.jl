cb13704e1dfbf62613684a421855a72d39921ac4
Paul Tiede <ptiede91@gmail.com>
2024-12-27 00:06:34 -0500

    Add some docstrings

--- src/multidomain.jl
+++ src/multidomain.jl
@@ -10,8 +10,28 @@ struct TaylorSpectral{N,P,T<:NTuple{N},F<:Real} <: FrequencyParams
     index::T
     freq0::F
 end
+
+"""
+    TaylorSpectral(param, index::NTuple{N}, freq0::Real) -> TaylorSpectral{N}
+
+Creates a frequency model that expands the parameter in a Taylor series defined by 
+    `param * exp(∑ index[i] * log(Fr / freq0)^i)`.
+i.e. an expansion in log(Fr / freq0) where Fr is the frequency of the observation, 
+`freq0` is the reference frequency, `param` is the parameter value at `freq0`.
+
+The `N` in index defines the order of the Taylor expansion. If `index` is a `<:Real`
+then the expansion is of order 1.
+"""
 TaylorSpectral(param, index::Real, freq0) = TaylorSpectral(param, (index,), freq0)
 
+"""
+    getparam(m, s::Symbol, p)
+
+Gets the parameter value `s` from the model `m` evaluated at the domain `p`. 
+This is similar to getproperty, but allows for the parameter to be a function of the 
+domain. Essentially is `m.s <: DomainParams` then `m.s` is evaluated at the parameter `p`.
+If `m.s` is not a subtype of `DomainParams` then `m.s` is returned.
+"""
 @inline function getparam(m, s::Symbol, p)
     ps = getproperty(m, s)
     return build_param(ps, p)
@@ -31,6 +51,19 @@ end
     return param
 end
 
+"""
+    @unpack_params a,b,c,... = m(p)
+
+Extracts the parameters `a,b,c,...` from the model `m` evaluated at the domain `p`.
+This is a macro that essentially lowers to 
+```julia
+a = getparam(m, :a, p)
+b = getparam(m, :b, p)
+...
+```
+For any model that may depend on a `DomainParams` type this macro should be used to 
+extract the parameters. 
+"""
 macro unpack_params(args)
     args.head != :(=) &&
         throw(ArgumentError("Expression needs to be of the form a, b, = c(p)"))
