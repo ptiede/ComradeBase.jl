3b668c2ca7cfd94cdd18b2f8b4079d9a0b35c8b9
Paul Tiede <ptiede91@gmail.com>
2026-02-05 21:39:59 -0500

    Add tests

--- test/reactant.jl
+++ test/reactant.jl
@@ -1,13 +1,23 @@
 using Reactant
 
 @testset "Reactant" begin
-    x = rand(54)
+    x = rand(54, 32)
     r = Reactant.to_rarray(x)
 
     @test @jit(ComradeBase.rgetindex(r, 10)) ≈ x[10]
     @jit(ComradeBase.rsetindex!(r, 3.14, 20))
     @test @allowscalar ComradeBase.rgetindex(r, 20) ≈ 3.14
 
+    @test ComradeBase.rgetindex(x, 1:10) ≈ x[1:10]
+    @test ComradeBase.rgetindex(x, :, 1) ≈ x[:, 1]
+    @test ComradeBase.rgetindex(x, 1) ≈ x[1]
+
+    @test @jit(ComradeBase.rgetindex(r, 1:10)) ≈ x[1:10]
+    @test @jit(ComradeBase.rgetindex(r, :, 1)) ≈ x[:, 1]
+    @test @jit(ComradeBase.rgetindex(r, 1:2, 1:2)) ≈ x[1:2, 1:2]
+
+    @jit(ComradeBase.rsetindex!(r, ones(10), 1:10))
+    @test ComradeBase.rgetindex(r, 1:10) ≈ ones(10)
 
     g = imagepixels(10.0, 10.0, 8, 8)
     go = @jit(identity(g))
